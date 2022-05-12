828c3e69327bc30d202db4e686f29b6c87196c96
jfharris <jfharris1983@gmail.com>
2022-05-11 23:28:18 -0500

    Added studentClass and const/interface printTeacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,3 +11,22 @@ interface Directors extends Teacher {
     numberOfReports: number;
 }
 
+const printTeacher: printTeacherFunction = (
+    firstName: string,
+    lastName: string
+) => `${firstName.charAt(0)}. ${lastName}`;
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+class StudentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string {
+        return 'Currently working';
+    };
+    displayName(): string {
+        return this.firstName;
+    };
+}
