81864e3603aac5f81c3d0e00322195ef4c8903a4
jfharris <jfharris1983@gmail.com>
2022-05-14 19:01:48 -0500

    Is this right?

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,12 +1,70 @@
-/** function isDirector (employee: Director | Teacher) */
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
 
-function executeWork (employee: Director | Teacher) {
-    if (isDirector(employee)) {
-        return employee.workDirectorTasks;
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface{
+    workFromHome(): string {
+      return 'Working from home';
     }
-    else {
-        return employee.workTeacherTasks;
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
     }
 }
 
+class Teacher implements TeacherInterface{
+    workFromHome(): string {
+      return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+      return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+      return 'Getting to work';
+    }
+}
+
+function createEmployee(salary: number | string): Teacher | Director {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    let message: string = '';
+    if (isDirector(employee)) {
+        message = employee.workDirectorTasks();
+    }
+    else {
+        message = employee.workTeacherTasks();
+    }
+    return message;
+}
+
 type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+    else if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+}
