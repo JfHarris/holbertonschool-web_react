b485fd6321d07f4c537267be026eb5ab523429f7
jfharris <jfharris1983@gmail.com>
2022-05-26 16:57:43 -0500

    Moved appends above button func

--- 0x04-Webpack/task_1/js/dashboard_main.js
+++ 0x04-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,13 @@
 const $ = require("jquery");
 const _ = require("lodash");
 
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append($button);
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+
 let count = 0;
 function updateCounter() {
   count++;
@@ -13,9 +20,3 @@ const $button = $("<button>Click here to get started</button>")
   trailing: false,
 })
 );
-
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append($button);
-$("body").append("<p id='count'></p>");
-$("body").append("<p>Copyright - Holberton School</p>");
