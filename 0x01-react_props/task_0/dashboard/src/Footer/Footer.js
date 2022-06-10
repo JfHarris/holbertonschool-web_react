f650cab0377dfa9e7115d81beca657c7dc70ad45
JFHarris <JFHarris1983@gmail.com>
2022-06-10 00:40:19 -0500

    Undoing erroneous changes

---
+++ 0x01-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+import React from 'react';
+import {getFullYear, getFooterCopy} from '../utils/utils.js';
+
+function Footer() {
+  return (
+  <div className="App-Footer">
+    <p>
+      Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+  </div>
+  );
+}
+
+export default Footer;
