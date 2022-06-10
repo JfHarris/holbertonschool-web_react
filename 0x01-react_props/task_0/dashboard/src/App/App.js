33f69286809058560ca68338511b7dd77cc44646
JFHarris <JFHarris1983@gmail.com>
2022-06-10 02:01:26 -0500

    GitHub problems

--- 0x01-react_props/task_0/dashboard/src/App/App.js
+++ 0x01-react_props/task_0/dashboard/src/App/App.js
@@ -1,24 +1,23 @@
-import logo from './holb_logo.jpg';
-import './App.css';
+import React, { Fragment } from "react";
+import Header from "../Header/Header";
+import Footer from '../Footer/Footer';
+import Login from "../Login/Login";
+import Notifications from '../Notifications/Notifications';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App.H1"> School dashboard</h1>
-      </header>
-      <body className="App-body">
-        <p>
-          Login to access the full dashboard
-        </p>
-      </body>
-      <footer className="App-footer">
-        <p>
-          Copyright 2020 - holberton School
-        </p>
-      </footer>
-    </div>
+    <React.Fragment>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <div className="App-body">
+          <Login />
+        </div>
+        <div className="App-footer">
+          <Footer />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
