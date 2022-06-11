7960627ee523f521beecb2d28321df740b0d6b92
JFHarris <JFHarris1983@gmail.com>
2022-06-11 00:54:05 -0500

    Still fixing push issues from last week

---
+++ 0x01-react_props/task_4/dashboard/src/App/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from '../Footer/Footer';
+import Login from "../Login/Login";
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from "prop-types";
+import './App.css';
+
+function App({ isLoggedIn }) {
+  return (
+    <>
+      <Notifications />
+      <div key="App">
+        <Header />
+      </div>
+      <div className="App-body">
+        {!isLoggedIn ? <Login /> : <CourseList />}
+      </div>
+      <div key="App-footer">
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+export default App;
