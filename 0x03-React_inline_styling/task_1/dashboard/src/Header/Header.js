0bc2f05244508b262ce885168f8229080b24bc2a
JFHarris <JFHarris1983@gmail.com>
2022-06-14 22:30:27 -0500

    Added aphrodite

--- 0x03-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x03-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,14 +1,27 @@
 import logo from '../assets/holb_logo.jpg';
-import './Header.css';
+import { css, StyleSheet } from 'aphrodite';
 import React from 'react';
 
 function Header() {
   return (
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className={css(styles.header)}>
+        <img src={logo} className={css(styles.headerImg)} />
         <h1 className="App.H1"> School dashboard</h1>
       </div>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: "center",
+    display: "flex",
+    color: "#e01d3f",
+    fontSize: "20px",
+  },
+
+  headerImg: {
+    width: "200px",
+  },
+});
 
 export default Header;
