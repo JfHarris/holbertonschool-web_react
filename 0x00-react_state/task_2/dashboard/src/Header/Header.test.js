70f991ea24b0721583d60e5ce4975c3f46b3b0c1
JFHarris <JFHarris1983@gmail.com>
2022-06-18 20:40:33 -0500

    Make login function, add tests, and update App

---
+++ 0x00-react_state/task_2/dashboard/src/Header/Header.test.js
@@ -0,0 +1,27 @@
+import Header from "./Header";
+import { shallow } from "enzyme";
+import React from "react";
+import { StyleSheetTestUtils } from "aphrodite";
+
+describe("<Header />", () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+  it("Header renders without crashing", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("Verify that the components render img", () => {
+    const wrapper = shallow(<Header />);
+    wrapper.update();
+    expect(wrapper.find("div img")).toHaveLength(1);
+  });
+  it("Verify that the components render h1", () => {
+    const wrapper = shallow(<Header />);
+    wrapper.update();
+    expect(wrapper.find("div h1")).toHaveLength(1);
+  });
+});
