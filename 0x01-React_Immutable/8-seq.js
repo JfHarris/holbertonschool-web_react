7b429b68bfa52ca0f52a5560cd7804f0a736c1b0
JFHarris <JFHarris1983@gmail.com>
2022-06-25 19:16:32 -0500

    Reorganized map parameters

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -2,8 +2,6 @@ import immutable from 'immutable';
 
 const { Seq } = immutable;
 
-const makeUppercase = (str) => str.charAt(0).toUpperCase() + str.slice(1);
-
 export default function printBestStudents(grades) {
   const studentsList = Seq(grades)
     .filter((student) => student.score > 70)
