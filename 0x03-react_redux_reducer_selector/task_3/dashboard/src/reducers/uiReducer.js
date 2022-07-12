ce36ba494c2c87f594598bfa53836cb6dc527ba7
JFHarris <JFHarris1983@gmail.com>
2022-07-12 00:44:09 -0500

    No idea

---
+++ 0x03-react_redux_reducer_selector/task_3/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,37 @@
+import { map } from 'immutable';
+
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from "../actions/uiActionTypes";
+
+export const basicState = {
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {},
+};
+
+const uiReducer = (state = Map(basicState), action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", true);
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", false);
+    case LOGIN_SUCCESS:
+      return state.set("isUserLoggedIn", true);
+    case LOGIN_FAILURE:
+      return state.set("isUserLoggedIn", false);
+    case LOGOUT:
+      return state.set("isUserLoggedIn", false);
+    default:
+      break;
+  }
+
+  return state;
+};
+
+export default uiReducer;
